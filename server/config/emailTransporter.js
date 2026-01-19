d2ccca475798df99e122f1bc71ee6d59b0e85b4e
= <tanmaybhansali07@gmail.com>
2026-01-19 20:00:06 +0530

    Send Email Fix

--- server/config/emailTransporter.js
+++ server/config/emailTransporter.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_APP_HOST,
-    port: process.env.EMAIL_APP_PORT,
+    port: Number(process.env.EMAIL_APP_PORT),
     secure: process.env.BUILD=="prod", 
     auth: {
       user: process.env.EMAIL_APP_USER,
